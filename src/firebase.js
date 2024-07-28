7da49b685a0954f9b89248bb91f06b419bc59080
ManpreetKaur06 <103641334+ManpreetKaur06@users.noreply.github.com>
2024-07-28 23:54:06 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC2ytcszZHkbD8SEdVr4yqFe-9GCQvsUyw",
-  authDomain: "fir-proj-react.firebaseapp.com",
-  projectId: "fir-proj-react",
-  storageBucket: "fir-proj-react.appspot.com",
-  messagingSenderId: "816296631532",
-  appId: "1:816296631532:web:2af7377bf5ab9a623a59c9" ,
-  databaseURL : "https://fir-proj-react-default-rtdb.firebaseio.com"
+  //in firebase
 };
 
 // Initialize Firebase
